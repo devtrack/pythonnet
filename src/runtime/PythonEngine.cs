c2344920db5ebfd3e88db8f93f04dd19bbdb4c1a
Romain Merlet <devtrack@users.noreply.github.com>
2026-01-14 18:00:54 +0100

    Parse pre-release Python versions for shutdown gating

--- src/runtime/PythonEngine.cs
+++ src/runtime/PythonEngine.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -151,10 +153,19 @@ namespace Python.Runtime
                 return new Version(0, 0);
             }
 
-            string versionPart = versionText.Split(' ')[0];
-            return Version.TryParse(versionPart, out Version? parsed)
-                ? parsed
-                : new Version(0, 0);
+            Match match = Regex.Match(versionText, @"^(\\d+)\\.(\\d+)(?:\\.(\\d+))?");
+            if (!match.Success)
+            {
+                return new Version(0, 0);
+            }
+
+            int major = int.Parse(match.Groups[1].Value, CultureInfo.InvariantCulture);
+            int minor = int.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+            int patch = match.Groups[3].Success
+                ? int.Parse(match.Groups[3].Value, CultureInfo.InvariantCulture)
+                : 0;
+
+            return new Version(major, minor, patch);
         }
 
         public static string BuildInfo
